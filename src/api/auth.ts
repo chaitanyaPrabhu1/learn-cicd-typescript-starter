b142cbb97d28eb020f5246b69c21720625f35092
chaitanyaPrabhu1 <chaitanyasprabhu@gmail.com>
2026-06-13 16:57:47 +0530

    fourth

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,10 +6,10 @@ export function getAPIKey(headers: IncomingHttpHeaders): string | null {
     return null;
   }
 
-  const splitAuth = authHeader.split(" ");
-  if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
-    return null;
-  }
+  // const splitAuth = authHeader.split(" ");
+  // if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
+  //   return null;
+  // }
 
-  return splitAuth[1];
+  // return splitAuth[1];
 }
